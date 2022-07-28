4cc9838b28cb14f0dab7245d80fe91cc8ef95468
setyawanyogi <setyawanyogi07@gmail.com>
2022-07-29 01:50:41 +0700

    update

--- parkir/create.php
+++ parkir/create.php
@@ -2,6 +2,7 @@
     require "../conn.php";
 
     // $connection = new mysqli("localhost", "root", "", "db_eparkir");
+    
     $id_kendaraan  = $_POST['id_kendaraan']; 
     $plat_nomor    = $_POST['plat_nomor'];
     $jam_masuk     = time('hh:mm');
@@ -12,9 +13,22 @@
     
     $result = mysqli_query($con, "INSERT INTO parkir SET id_kendaraan='$id_kendaraan', plat_nomor='$plat_nomor', jam_masuk=NOW(), jam_keluar=NULL, tgl=NOW(), status='$status'");
     
+    $id = mysqli_insert_id($con);
+    $quer = mysqli_query($con, "SELECT parkir.id_parkir, kendaraan.jenis_kendaraan, parkir.plat_nomor, parkir.jam_masuk, parkir.jam_keluar, parkir.tgl, parkir.barcode, parkir.status, kendaraan.biaya FROM parkir INNER JOIN kendaraan ON parkir.id_kendaraan=kendaraan.id_kendaraan WHERE parkir.status = 'Parkir' AND id_parkir='$id'");
+    
     if($result){
+        $row = mysqli_fetch_array($quer, MYSQLI_ASSOC);
         echo json_encode([
-            'message' => 'Data input successfully'
+            'message'       => 'Data input successfully',
+            'id_parkir'     => $id,
+            'plat_nomor'    => $row["plat_nomor"],
+            'jam_masuk'     => $row["jam_masuk"],
+            'jam_keluar'    => $row["jam_keluar"],
+            'tgl'           => $row["tgl"],
+            'status'        => $row["status"],
+            'biaya'         => $row["biaya"],
+            // 'data' => $courses
+
         ]);
     }else{
         echo json_encode([
--- parkir/update.php
+++ parkir/update.php
@@ -3,16 +3,27 @@
     
     $id_kendaraan  = $_POST['id_kendaraan']; 
     $plat_nomor    = $_POST['plat_nomor'];
-    $jam_masuk     = date('yyyy-mm-dd hh:mm');
-    $jam_keluar     = date('yyyy-mm-dd hh:mm');
+    $jam_masuk     = time('hh:mm');
+    $jam_keluar    = time('hh:mm');
+    $tgl           = date('dd-mm-yyyy');
     $status        = $_POST['status']; 
-    $id_parkir         = $_POST['id_parkir'];
         
-    $result = mysqli_query($con, "UPDATE parkir SET id_kendaraan='$id_kendaraan', plat_nomor='$plat_nomor', jam_masuk=='$jam_masuk', jam_keluar=='$jam_keluar', status='$status' WHERE id_parkir='$id_parkir'");
-        
+    $result = mysqli_query($con, "UPDATE parkir SET id_kendaraan='$id_kendaraan', plat_nomor='$plat_nomor', jam_masuk='$jam_masuk', jam_keluar=NOW(), tgl='$tgl', status='Selesai' WHERE id_parkir='$id_parkir'");
+    
+    $id = mysqli_insert_id($con);
+    $quer = mysqli_query($con, "SELECT parkir.id_parkir, kendaraan.jenis_kendaraan, parkir.plat_nomor, parkir.jam_masuk, parkir.jam_keluar, parkir.tgl, parkir.barcode, parkir.status, kendaraan.biaya FROM parkir INNER JOIN kendaraan ON parkir.id_kendaraan=kendaraan.id_kendaraan WHERE parkir.status = 'Parkir' AND id_parkir='$id'");
+
     if($result){
+       $row = mysqli_fetch_array($quer, MYSQLI_ASSOC);
         echo json_encode([
-            'message' => 'Data edit successfully'
+            'message'       => 'Data input successfully',
+            'id_parkir'     => $id,
+            'plat_nomor'    => $row["plat_nomor"],
+            'jam_masuk'     => $row["jam_masuk"],
+            'jam_keluar'    => $row["jam_keluar"],
+            'tgl'           => $row["tgl"],
+            'status'        => $row["status"],
+            'biaya'         => $row["biaya"],
         ]);
     }else{
         echo json_encode([
